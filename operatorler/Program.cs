327f5b16e3b1544527d0a4fa0daa133f873aa7a7
Ugur Metin <ugurmetin@windowslive.com>
2022-02-27 22:28:38 +0300

    Operatörler

---
+++ operatorler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace consoleProgramlama
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Selam");
+        }
+    }
+}
