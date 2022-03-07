b2eb008fc66361b1e282cfb833a92c19373e5e84
Ugur Metin <ugurmetin@windowslive.com>
2022-03-07 22:40:17 +0300

    PatikaDev ödev ve alıştırmalar

--- arrays/Program.cs
+++ arrays/Program.cs
@@ -6,39 +6,44 @@ namespace arrays
     {
         static void Main(string[] args)
         {
-            //Dizi tamınlamaları
-            string[] colors = new string[5];
+            // //Dizi tamınlamaları
+            // string[] colors = new string[5];
 
-            string[] animals = { "Kedi", "Köpek", "Kuş", "Aslan" };
+            // string[] animals = { "Kedi", "Köpek", "Kuş", "Aslan" };
 
-            int[] dizi;
-            dizi = new int[5];
+            // int[] dizi;
+            // dizi = new int[5];
 
-            //Dizilere değer atama ve erişim
-            colors[0] = "Kırmızı";
-            dizi[3] = 47;
+            // //Dizilere değer atama ve erişim
+            // colors[0] = "Kırmızı";
+            // dizi[3] = 47;
 
-            Console.WriteLine(animals[3]);
-            Console.WriteLine(dizi[3]);
-            Console.WriteLine(colors[0]);
+            // Console.WriteLine(animals[3]);
+            // Console.WriteLine(dizi[3]);
+            // Console.WriteLine(colors[0]);
 
-            //Döngüler ile Dizi kullanımı
-            Console.WriteLine("Lütfen dizinin eleman sayısını giriniz: ");
-            int diziUzunlugu = int.Parse(Console.ReadLine());
-            int[] sayiDizisi = new int[diziUzunlugu];
+            // //Döngüler ile Dizi kullanımı
+            // Console.WriteLine("Lütfen dizinin eleman sayısını giriniz: ");
+            // int diziUzunlugu = int.Parse(Console.ReadLine());
+            // int[] sayiDizisi = new int[diziUzunlugu];
 
-            for (int i = 0; i < diziUzunlugu; i++)
-            {
-                Console.WriteLine("Lütfen {0}. sayısı giriniz: ", i + 1);
-                sayiDizisi[i] = int.Parse(Console.ReadLine());
+            // for (int i = 0; i < diziUzunlugu; i++)
+            // {
+            //     Console.WriteLine("Lütfen {0}. sayısı giriniz: ", i + 1);
+            //     sayiDizisi[i] = int.Parse(Console.ReadLine());
 
-                int toplam = 0;
-                foreach (var sayi in sayiDizisi)
-                {
-                    toplam += sayi;
-                }
-                Console.WriteLine("Ortalama: " + toplam / diziUzunlugu);
-            }
+            //     int toplam = 0;
+            //     foreach (var sayi in sayiDizisi)
+            //     {
+            //         toplam += sayi;
+            //     }
+            //     Console.WriteLine("Ortalama: " + toplam / diziUzunlugu);
+            // }
+
+            int a = 4;
+            int b = 6;
+            b = a++;
+            Console.WriteLine(++b);
         }
     }
 }
---
+++ arraysTwo/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace arraysTwo
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            //Sort sıralama işlemi
+            int[] sayiDizisi = { 8, 23, 12, 47, 52, 5, 1, 21 };
+
+            Console.WriteLine("Sırasız Dizi");
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+            System.Console.WriteLine("Sıralı Dizi");
+            Array.Sort(sayiDizisi);
+            foreach (var sayi in sayiDizisi)
+            {
+                System.Console.WriteLine(sayi);
+            }
+
+
+            //Clear Dizi içerisinde verilen veri kadar verinin yerine o ile doldurur.
+
+            System.Console.WriteLine("Array Clear");
+            //sayiDizisi elemanlarını kullanarak 2. indexten itibaren 2 tane elemanı 0'lar.
+
+            Array.Clear(sayiDizisi, 2, 2);
+
+            foreach (var sayi in sayiDizisi)
+            {
+                System.Console.WriteLine(sayi);
+            }
+
+            //Reverse verilen diziyi sondan başa doğru sıralar
+            System.Console.WriteLine("Array Reverse");
+
+            Array.Reverse(sayiDizisi);
+            foreach (var sayi in sayiDizisi)
+            {
+                System.Console.WriteLine(sayi);
+            }
+
+            //IndexOf Verilen dizinin verilen elemanının indexini getirir.
+            System.Console.WriteLine("Array IndexOf");
+
+            System.Console.WriteLine(Array.IndexOf(sayiDizisi, 47));
+
+
+            //Resize Dizileri yeniden boyutlandırmak için kullanılır.
+            System.Console.WriteLine("Array Resaize");
+            Array.Resize<int>(ref sayiDizisi, 9);
+            sayiDizisi[8] = 99;
+
+            foreach (var sayi in sayiDizisi)
+            {
+                System.Console.WriteLine(sayi);
+            }
+        }
+    }
+}
